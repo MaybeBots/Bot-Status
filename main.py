0a3cbea52e646b491028facd03e4152096e994bf
MAYBE SURAJ <94737162+Surajx21@users.noreply.github.com>
2023-05-21 11:55:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ MESSAGE_ID = int(os.getenv("MESSAGE_ID"))
 TIME_ZONE = os.getenv("TIME_ZONE")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 
-bot.start()
+
 
 
 async def main():
@@ -59,4 +59,4 @@ async def main():
             await asyncio.sleep(900)
 
 
-asyncio.run(main())
+bot.run(main())
